02b5eca396d77b7757503036d46008a4577f99b9
ezreal <tornike.zhizhiashvili@geolab.edu.ge>
2018-06-11 01:09:10 +0400

    no message

--- js/portf.js
+++ js/portf.js
@@ -1,25 +1,5 @@
 
 
-
-//     	x = 0;
-
-// // function html(){
-
-// // $(".skil_html span").text(x + "%")
-
-// // 	// if (x === 88) {
-
-// // 	// 	clearInterval(stop)
-
-// // 	// }
-// // 	x++;
-// // }
-// var stop =  setInterval(html,15);
-//         $(document).off('scroll');
-
-
-//navbar
-
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
@@ -58,7 +38,7 @@ $(document).ready(function(){
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top - 98
+        scrollTop: $(hash).offset().top - 52
       }, 800, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
@@ -92,8 +72,6 @@ var pos =  $(this).scrollTop();
 
 
 
-// var clasArr = ["skil_html","skil_css","skil_bootstrap","skil_javascript","skil_jquery"];
-
 
 // $(document).on('scroll',function() {
 
@@ -101,35 +79,7 @@ var pos =  $(this).scrollTop();
 
 // console.log(pos)
 
- //    if (pos >955 && $(document).width() > 992 ) {
-
-
-	// 	$('.skil_progress_bar').each(function(index,current){
-	// 		$(this).addClass(clasArr[index]);
-
-	// 		});
-
-
-
- //    		setInterval(function(){
-
-	// 	$(".skil_progress_bar").each(function(){
-	// 	var child= $(this).width();
-	// 	var parent=$(this).parent().width();
-	// 	var proc = Math.floor(((child / parent) * 100) +1);
-	// 	$(this).text(proc + "%");
-
-	// 	})
-
-
-
-	// },30);
-
-
-//     };
-
-
-//     if (pos > 850 && $(document).width() < 992 && $(document).width() > 576 ) {
+//     if (pos >955 && $(document).width() > 992 ) {
 
 
 // 		$('.skil_progress_bar').each(function(index,current){
@@ -154,146 +104,6 @@ var pos =  $(this).scrollTop();
 // 	},30);
 
 
-//     }
-
-//     if (pos > 1035 && $(document).width() < 576 ) {
-
-
-// 		$('.skil_progress_bar').each(function(index,current){
-// 			$(this).addClass(clasArr[index]);
-
-// 			});
-
-
-
-//     		setInterval(function(){
-
-// 		$(".skil_progress_bar").each(function(){
-// 		var child= $(this).width();
-// 		var parent=$(this).parent().width();
-// 		var proc = Math.floor(((child / parent) * 100) +1);
-// 		$(this).text(proc + "%");
-
-// 		})
-
-
-
-// 	},30);
-
-
-//     }
-
-
-// });
-
-
-// about animaciebis
-
-// $(document).on('scroll',function() {
-
-// var pos =  $(this).scrollTop();
-
-// console.log(pos)
-
-//     if (pos >285 && $(document).width() > 992 ){
-
-//     	$(".img_box").addClass("bounceInLeft");
-//     	$(".img_row .about_text").addClass("bounceInRight");
-
-
-//     }else if (pos >225 && $(document).width() < 992 && $(document).width() > 576 ) {
-
-//     		$(".img_box").addClass("bounceInLeft");
-//     	$(".img_row .about_text").addClass("bounceInRight");
-
-
-//     }else if (pos >20 && $(document).width() < 576) {
-
-//     	$(".img_box").addClass("bounceInLeft");
-//     	$(".img_row .about_text").addClass("bounceInRight");
-//     }
-
-// experience animation
-
-// if (pos >1550 && $(document).width() > 992 ){
-
-//     	$(".time_col").addClass("rollIn");
-//     	$(".exp_info").addClass("rollIn");
-
-
-//     }else if (pos >1530 && $(document).width() > 576 ) {
-
-
-//     	$(".time_col").addClass("rollIn");
-//     	$(".exp_info").addClass("rollIn");
-
-//     }else if (pos >1690 && $(document).width() < 576 ) {
-
-//     		$(".time_col").addClass("rollIn");
-//     	$(".exp_info").addClass("rollIn");
-//     }
-
-    
-//portf animace
-
-
-
-
-// if (pos >2251 && $(document).width() > 992 ){
-
-//     	$(".port_box").addClass("zoomInDown");
-    	
-//     }else if (pos >2441 && $(document).width() < 992 && $(document).width() > 576) {
-
-//     	$(".port_box").addClass("zoomInDown");
-//     }else if (pos >2750 && $(document).width() < 576) {
-
-//     	$(".port_box").addClass("zoomInDown");
-
-//     }
-
-
-
-// if (pos >3268 && $(document).width() > 992 ){
-
-//     	$(".contact_mail").addClass("bounceInLeft");
-//     	$(".contact_right").addClass("bounceInRight");
-
-
-//     }else if (pos >5664 && $(document).width() < 992 && $(document).width() > 576 ) {
-
-// 		$(".contact_mail").addClass("bounceInLeft");
-//     	$(".contact_right").addClass("bounceInRight");
-
-
-//     }else if (pos >4808 && $(document).width() < 576) {
-
-//     		$(".contact_mail").addClass("bounceInLeft");
-//     	$(".contact_right").addClass("bounceInRight");
-//     }
-
-
-
-//     if (pos >100 ) {
-
-//     	$(".logo img").css("width","40px")
-//     }else if (pos < 100) {
-
-//     	$(".logo img").css("width","60px")
-//     }
-
-
-// })
-//     if (pos >100 ) {
-
-//     	$(".logo img").css("width","40px")
-//     }else if (pos < 100) {
-
-//     	$(".logo img").css("width","60px")
-//     }
-
-// $('.skil_progress_bar').addClass("skil_html")
-			
 
 		
 // var $animation_elements = $('.img_box');
@@ -386,6 +196,7 @@ function check_if_in_view3() {
 
 
 
+
 $(window).on('scroll', check_if_in_view4);
 $(window).on('scroll resize', check_if_in_view4);
 $(window).trigger('scroll');
@@ -419,7 +230,7 @@ $(window).on('scroll', check_if_in_view5);
 $(window).on('scroll resize', check_if_in_view5);
 $(window).trigger('scroll');
 
-var clasArr = ["skil_html","skil_css","skil_bootstrap","skil_javascript","skil_jquery"];
+// var clasArr = ["skil_html","skil_css","skil_bootstrap","skil_javascript","skil_jquery"];
 
 function check_if_in_view5() {
   var window_height = $(window).height();
@@ -436,8 +247,8 @@ function check_if_in_view5() {
     if ((element_bottom_position >= window_top_position) &&
         (element_top_position <= window_bottom_position)) {
 
-      $('.skil_progress_bar').addClass('skil_html');
-      
+      // $('.skil_progress_bar').addClass('skil_html');
+    var clasArr = ["skil_html","skil_css","skil_bootstrap","skil_javascript","skil_jquery","skil_photoshop","skil_illustrator"];  
      
 		$('.skil_progress_bar').each(function(index,current){
 			$(this).addClass(clasArr[index]);
@@ -450,21 +261,13 @@ function check_if_in_view5() {
 		var parent=$(this).parent().width();
 		var proc = Math.floor(((child / parent) * 100) +1);
 		$(this).text(proc + "%");
-
-		})
-
-
-
-	},30);
+			})
+		},30);
 
 
     }
   });
 }
-
-
-
-
 
 
 
